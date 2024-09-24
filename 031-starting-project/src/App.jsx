1e66d0b0e02199360248ad75e3c21e008fb6ff59
German Vargas <german.vargas@episource.com>
2024-09-24 15:02:34 -0500

    starting project

---
+++ 031-starting-project/src/App.jsx
@@ -0,0 +1,12 @@
+import Post from "./components/Post";
+
+function App() {
+  return (
+    <main>
+      <Post author="german" body="reactjs is awesome" />
+      <Post author="valentina " body="reactjs is amazing" />
+    </main>
+  );
+}
+
+export default App;
