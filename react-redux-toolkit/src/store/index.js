23b8a7988705e27efefdb06243da716669c5a889
Captain Mcwiise <mcwiis.cap@gmail.com>
2024-09-29 15:26:52 -0500

    Form submission and outputting list of data

--- react-redux-toolkit/src/store/index.js
+++
@@ -1,8 +0,0 @@
-import { createSlice, configureStore } from "@reduxjs/toolkit";
-import counterSlice from "./counter";
-import authSlice from "./auth";
-
-const store = configureStore({
-  reducer: { counter: counterSlice.reducer, auth: authSlice.reducer },
-});
-export default store;
