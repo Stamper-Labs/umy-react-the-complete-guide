bd3ce1ff8f47cf92de1e1be56a725e265aff089a
Captain Mcwiise <mcwiis.cap@gmail.com>
2024-10-12 19:05:11 -0500

    using actions to sumbit data in a form

--- starting-project/src/components/PostsList.jsx
+++ starting-project/src/components/PostsList.jsx
@@ -1,50 +1,19 @@
-import { useState, useEffect } from "react";
-
-import Post from "./Post";
-import NewPost from "../routes/NewPost";
-import Modal from "./Modal";
 import classes from "./PostsList.module.css";
+import { useLoaderData } from "react-router-dom";
+import Post from "./Post";
 
 function PostsList() {
-  const [posts, setPosts] = useState([]);
-  const [isFetching, setFetching] = useState("");
-
-  useEffect(() => {
-    async function fetchPosts() {
-      setFetching(true);
-      const response = await fetch("http://localhost:8080/posts");
-      const data = await response.json();
-      setPosts(data.posts);
-      setFetching(false);
-    }
-    fetchPosts();
-  }, []);
-
-  function addPostHandler(postData) {
-    console.log("data to store: ", JSON.stringify(postData));
-    fetch("http://localhost:8080/posts", {
-      method: "POST",
-      body: JSON.stringify(postData),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-    setPosts(() => {
-      return [postData, ...posts];
-    });
-  }
-
+  const posts = useLoaderData();
   return (
     <>
-      {!isFetching && (
+      {
         <ul className={classes.posts}>
           {posts.map((post) => (
             <Post key={post.body} author={post.author} body={post.body} />
           ))}
         </ul>
-      )}
-      {!isFetching && posts.length === 0 && <h1>there are no posts</h1>}
-      {isFetching && <p>Loading posts...</p>}
+      }
+      {posts.length === 0 && <h1>there are no posts</h1>}
     </>
   );
 }
