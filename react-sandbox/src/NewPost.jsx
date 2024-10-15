219b38d86a8708c5c38391172c0514da6e91d8c5
Captain Mcwiise <mcwiis.cap@gmail.com>
2024-10-14 21:27:12 -0500

    react sandbox project

--- react-sandbox/src/NewPost.jsx
+++ react-sandbox/src/NewPost.jsx
@@ -1,26 +1,34 @@
 import classes from "./NewPost.module.css";
+import { useState } from "react";
 
 /**
- * <NewPost> component renders a form with a textarea and an input text.
+ * <NewPost> component renders a form with a textarea and an input texxt
  *
- * The component also registers an event listener that prints out to the console
- * whenever the user types into the textarea.
+ * The component makes use of the useState() hook
+ * to keep the state of a variable on every component's re-render:
  *
+ * 1. Registers an event listner that reacts to input changes in the text area.
+ * 2. Updates the state of  enteredBody variable and triggers a re-render action.
+ * 3. Prints out the current state of enteredBody in a <p>.
  * @returns {JSX.Element} a <form>
  */
 function NewPost() {
-  // The event listener receives the event
-  // that was triggered by the html element
+  const [enteredBody, setEnteredBody] = useState("");
+
   function changeBodyHandler(event) {
-    console.log(event.target.value);
+    // the re-render action on the entire component
+    // is triggered by calling the update function.
+    setEnteredBody(event.target.value);
   }
 
   return (
     <form className={classes.form}>
       <p>
         <label htmlFor="body">Text</label>
+        {/* the event listener is registered here*/}
         <textarea id="body" required rows={3} onChange={changeBodyHandler} />
       </p>
+      <p>{enteredBody}</p>
       <p>
         <label htmlFor="name">Your name</label>
         <input type="text" id="name" required />
