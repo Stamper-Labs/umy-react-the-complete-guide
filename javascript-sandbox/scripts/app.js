c87b92021f174fbb15b7d9ae8a4d6cf81fd8e317
Captain Mcwiise <mcwiis.cap@gmail.com>
2024-10-20 11:24:47 -0500

    react sandbox project

--- javascript-sandbox/scripts/app.js
+++ javascript-sandbox/scripts/app.js
@@ -1,13 +1,4 @@
-const hobbies = ["work", "drink"];
-const fname = "german";
-
-function modifier(hobbies, fname) {
-  hobbies.push("sleep");
-  fname = "valentina";
-  console.log("print out from modifier function: ", fname);
+function cb() {
+  console.log("tales");
 }
-
-modifier(hobbies, fname);
-
-console.log(hobbies);
-console.log(fname);
+setTimeout(cb);
