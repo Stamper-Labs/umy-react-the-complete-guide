636f6243d98a55693457be2c6ff3b9a9cb87248e
German Vargas <german.vargas@episource.com>
2025-05-19 16:26:18 -0500

    import and export keywords

--- 02-JAVASCRIPT-REFRESHER/js-sandobx/scripts/app.js
+++ 02-JAVASCRIPT-REFRESHER/js-sandobx/scripts/app.js
@@ -1,5 +1,12 @@
-function add(a, b) {
-  return a + b;
-}
-const result = add(1, "2");
-console.log(result);
+// import and give a randon name to the defatult export
+import apiKey from "./util";
+
+// import the username variable
+import { username } from "./util";
+
+// import the contrasena variable and give it an alias
+import { contrasena as password } from "./util";
+
+console.log(apiKey);
+console.log(username);
+console.log(password);
